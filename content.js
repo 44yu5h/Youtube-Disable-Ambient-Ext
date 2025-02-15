cc91e3a146f1d0abac289bf422265c7d07439177
44yu5h <meet44yu5h@gmail.com>
2025-02-16 04:37:12 +0530

    first commit: disable on page load

---
+++ content.js
@@ -0,0 +1,30 @@
+let isMenuLoaded = false;
+const settingsButton = document.querySelector('.ytp-settings-button');
+
+const observer = new MutationObserver(() => {
+    if (settingsButton) {
+        if (!isMenuLoaded){
+            console.log("-----------Opening settings menu");
+            // Menu loads in DOM only when settings is opened - open and close quickly
+            settingsButton.click();
+            settingsButton.click();
+            isMenuLoaded = true;
+        }
+        let ambientMenuItem = Array.from(document.querySelectorAll('.ytp-menuitem')).find(el => el.querySelector('.ytp-menuitem-label')?.textContent === "Ambient mode");
+        if (ambientMenuItem) {
+            console.log("Ambient Mode option found.");
+            let toggleState = ambientMenuItem.getAttribute("aria-checked");
+
+            if (toggleState !== "false") {
+                ambientMenuItem.click();
+                console.log("-----------Turning OFF!");
+            } else {
+                console.log("-----------Already OFF");
+            }
+
+            observer.disconnect();
+        }
+    }
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
